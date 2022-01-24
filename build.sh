0267088f585a434fc2808874c5ec4e71f15df149
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2022-01-24 20:32:32 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ ls -d linux*.deb
 
 pwd
 
-sudo curl -F document=@linux-image-5.6.9_5.6.9-1_amd64.deb https://api.telegram.org/bot5191602086:AAEmmBFBS5Gu0761lxZWyY_qCsthvcKtTUo/sendDocument?chat_id="1987074996"
+
 
 # setup TG message and build posts
 telegram_message() {
@@ -23,3 +23,6 @@ telegram_message "
 	*Date:* \`$(date +"%d-%m-%Y %T")\`
 	*✅ Build finished after seconds*"  &> /dev/null
 
+sudo curl -F document=@linux-image-5.6.9_5.6.9-1_amd64.deb https://api.telegram.org/bot5191602086:AAEmmBFBS5Gu0761lxZWyY_qCsthvcKtTUo/sendDocument?chat_id="1987074996"
+
+exit [n]
